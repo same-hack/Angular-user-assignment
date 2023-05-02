37cd1107642a8b75b4b4ddde1f7e9d24728d1ec7
same-hack <samehack.info@gmail.com>
2023-05-02 12:38:55 +0900

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'selectUser';
+
+  userList = [
+    { id: 1, name: 'サメハック' },
+    { id: 2, name: 'ねこハック' },
+    { id: 3, name: 'いぬハック' },
+  ];
+
+  todoList = [
+    { title: '事務所の片付け', userId: 1 },
+    { title: '荷物の発送', userId: 2 },
+    { title: '料金支払い', userId: 3 },
+    { title: '経費精算', userId: '' },
+  ];
+
+  // ユーザIDを取得
+  myFunc(event: any) {
+    console.log('event', event);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatSelectModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
